b87b489d2b8ea2d0c624841cb0eaf8bd328de501
mahrokh <mahrokh.nz@gmail.com>
2024-07-02 12:20:56 +0330

    feat: complete downloading resume

--- src/download.ts
+++ src/download.ts
@@ -4,10 +4,9 @@ import 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundl
 
 html2pdf()
     .set({
-        margin: 0,
+        margin: [0, -2, 0, -35],
         filename: 'Nabizadeh_CV.pdf',
-        html2canvas: {scale: 2},
-        image: {type: 'svg', quality: 0.98},
+        html2canvas: {scale: 1},
         jsPDF: {unit: 'mm', format: 'a4', orientation: 'portrait', no_copy: false},
     })
     .from(resume)
