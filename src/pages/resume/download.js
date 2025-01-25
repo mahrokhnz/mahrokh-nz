fb13764b3d89f6233d9a1545840c7e3ea0dd90da
nabizadeh-m <mahrokh.nabizadeh4@gmail.com>
2025-01-25 12:13:23 +0330

    refactor: download resume

--- src/pages/resume/download.js
+++ src/pages/resume/download.js
@@ -1,11 +1,11 @@
 const resume = document.querySelector(".resumeContainer");
 
-// html2pdf()
-//     .set({
-//         margin: [0, 0, -15, 0],
-//         filename: 'Nabizadeh_CV.pdf',
-//         html2canvas: {scale: 1},
-//         jsPDF: {unit: 'mm', format: 'a4', orientation: 'portrait', no_copy: false},
-//     })
-//     .from(resume)
-//     .save();
+html2pdf()
+    .set({
+        margin: [-1, -2, -4, 10],
+        filename: 'Nabizadeh_CV.pdf',
+        html2canvas: {scale: 1},
+        jsPDF: {unit: 'mm', format: 'a4', orientation: 'portrait', no_copy: false},
+    })
+    .from(resume)
+    .save();
