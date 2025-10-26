6ef865f4417ff01dd22d9be1c66aa049f4a6c37a
mahrokhnz <mahrokh.nz@gmail.com>
2025-10-26 10:46:41 +0330

    add blogs in menu

--- components/menu/page.tsx
+++ components/menu/page.tsx
@@ -1,19 +1,21 @@
 import styles from "./page.module.sass";
 import Link from 'next/link';
 import cls from "@/utils/class_names";
+import data from '@/data/db.json'
 
 interface MenuProps {
     isOpen?: boolean;
     isFooter?: boolean;
 }
 
-function Menu({ isOpen = false, isFooter = false }: MenuProps) {
+function Menu({isOpen = false, isFooter = false}: MenuProps) {
+    const {menu} = data
+
     return (
-        <ul className={cls(!isFooter ? styles.headerMenu : styles.footerMenu ,styles.menu, isOpen && styles.open)}>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/about">About</Link></li>
-            <li><Link href="/projects">Projects</Link></li>
-            <li><Link href="/contact">Contact</Link></li>
+        <ul className={cls(!isFooter ? styles.headerMenu : styles.footerMenu, styles.menu, isOpen && styles.open)}>
+            {menu.map((item) => (
+                <li key={item.id}><Link href={item.link}>{item.title}</Link></li>
+            ))}
         </ul>
     );
 }
