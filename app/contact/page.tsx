e14bf029ce6328948e0baa3010ee5da3635ef983
mahrokhnz <mahrokh.nz@gmail.com>
2026-07-28 13:38:52 +0330

    make app pwa

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,37 +1,40 @@
+import type {Metadata} from "next";
 import styles from './page.module.sass';
 import Container from "@/components/container/page";
 import {MdEmail} from "react-icons/md";
 import Canvas from "@/components/constellation/page";
-import MetadataComponent from "@/utils/client-metadata";
 import * as React from "react";
 import SectionTitle from "@/components/section_title/page";
 import Form from "@/app/contact/_components/form/page";
+import metadataCreator from "@/utils/server-metadata";
+
+export const metadata: Metadata = metadataCreator(
+    'Contact',
+    'Get in touch with MAHrokh, a skilled Front-End Developer. Contact me for inquiries about projects, collaborations, or any questions regarding web development.',
+);
 
 function ContactMe(){
     return (
-        <>
-            <MetadataComponent title='Contact' description={'Get in touch with MAHrokh, a skilled Front-End Developer. Contact me for inquiries about projects, collaborations, or any questions regarding web development.'} />
-            <main className={styles.main}>
-                <Canvas onlyStarts={true}/>
-                <section className={styles.contactWrapper}>
-                    <Container className={styles.contact}>
-                        <div className={styles.rowWrapper}>
-                            <div className={styles.content}>
-                                <SectionTitle text='Contact Me' alignment='flex-start' className={styles.title} />
-                                <p className={styles.description}>
-                                    Feel free to reach out to me for any inquiries, collaborations, or just to say hi! Whether you have a question, a project idea, or simply want to connect, I&#39;m always eager to hear from you.
-                                </p>
-                                <div className={styles.email}>
-                                    <MdEmail className={styles.emailIcon}/>
-                                    <a href="mailto:mahrokh.nz@gmail.com">Mahrokh.nz@gmail.com</a>
-                                </div>
+        <main className={styles.main}>
+            <Canvas onlyStarts={true}/>
+            <section className={styles.contactWrapper}>
+                <Container className={styles.contact}>
+                    <div className={styles.rowWrapper}>
+                        <div className={styles.content}>
+                            <SectionTitle text='Contact Me' alignment='flex-start' className={styles.title} />
+                            <p className={styles.description}>
+                                Feel free to reach out to me for any inquiries, collaborations, or just to say hi! Whether you have a question, a project idea, or simply want to connect, I&#39;m always eager to hear from you.
+                            </p>
+                            <div className={styles.email}>
+                                <MdEmail className={styles.emailIcon}/>
+                                <a href="mailto:mahrokh.nz@gmail.com">Mahrokh.nz@gmail.com</a>
                             </div>
-                            <Form />
                         </div>
-                    </Container>
-                </section>
-            </main>
-        </>
+                        <Form />
+                    </div>
+                </Container>
+            </section>
+        </main>
     );
 }
 
