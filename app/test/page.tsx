6204214e38ade625361573e8bf4cdd88dc88a5a6
mahrokhnz <mahrokh.nz@gmail.com>
2026-08-25 10:07:09 +0330

    delete extra

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import Resume from "@/components/resume/page";
-
-export default function ResumePage() {
-    return (
-        <div className='pt-50 pl-50'>
-            <Resume/>
-        </div>
-    );
-}
